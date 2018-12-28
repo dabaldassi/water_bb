679a9eeaa709554b424954bb1768b10578f4489a
pixel <david.baldassin@etu.uca.fr>
2018-12-28 17:30:32 +0100

    init project

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (cmake-ide-build-dir . "~/Documents/ISIMA/pixel/water_bb/build")))
--- src/water_bb/main.cpp
+++ src/water_bb/main.cpp
@@ -1,15 +1,22 @@
 #include <gameEngine/game.h>
+#include <gameEngine/actor/controlable/controlable.h>
+
+#define WIDTH 1920
+#define HEIGHT 1080
 
 void generate(Stage * stage)
 {
-
+  constexpr int NB_BLOCK_W = 21;
+  constexpr int NB_BLOCK_H = 8;
+  constexpr float SIZE_BLOCK_W = WIDTH/NB_BLOCK_W;
+  constexpr float SIZE_BLOCK_H = HEIGHT/NB_BLOCK_H;
+  
 }
 
 int main()
 {
-  int w = 1920, h = 1080;
-  Game game(w,h,"Super Game");
-
+  Game game(WIDTH,HEIGHT,"Water Boat Battle");
+  
   game.run(generate);
   
   return 0;
